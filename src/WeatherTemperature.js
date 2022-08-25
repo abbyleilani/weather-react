b666d5cc6f15ebc05f017cc587b751c2e32edf8a
abbyleilani <abbyleilanibaker@gmail.com>
2022-08-25 11:38:30 -0700

    Updated weather conversion in WeatherTemperature component
    Track the current weather in imperial and metric now.

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -4,19 +4,24 @@ import "./WeatherTemperature.css";
 export default function WeatherTemperature(props) {
   const [unit, setUnit] = useState("celsius");
 
-  function convertoFahrenheit(event) {
+  function showFahrenheit(event) {
     event.preventDefault();
     setUnit("fahrenheit");
   }
 
+  function showCelsius(event) {
+    event.preventDefault();
+    setUnit("celsius");
+  }
+
   if (unit === `celsius`) {
     return (
-      <div className="weatherTemperature">
+      <div className="WeatherTemperature">
         <h2>
           <span className="temperature">{Math.round(props.celsius)} </span>
           <span className="unit">
             °C|{" "}
-            <a href="/" onClick={convertoFahrenheit}>
+            <a href="/" onClick={showFahrenheit}>
               °F
             </a>{" "}
           </span>
@@ -24,15 +29,16 @@ export default function WeatherTemperature(props) {
       </div>
     );
   } else {
+    let fahrenheit = (props.celsius * 9) / 5 + 32;
     return (
       <div className="weatherTemperature">
         <h2>
-          <span className="temperature">{Math.round(props.celsius)} </span>
+          <span className="temperature">{Math.round(fahrenheit)} </span>
           <span className="unit">
-            <a href="/" onClick={convertoFahrenheit}>
+            <a href="/" onClick={showCelsius}>
               °C
             </a>
-            | °F{" "}
+            | °F
           </span>
         </h2>
       </div>
