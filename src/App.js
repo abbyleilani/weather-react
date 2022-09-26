07c1f7a1e0a963ad915dea51c83d82fead4b30c6
abbyleilani <abbyleilanibaker@gmail.com>
2022-09-26 13:15:01 -0700

    updated background color

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import "./App.css";
+import "./images/WeatherLogo.png";
 import Weather from "./Weather";
 
 function App() {
   return (
     <div className="App">
+      <img src="./images/WeatherLogo.png" alt="logo" />
       <header>
         <div className="container">
           <Weather defaultCity="New York" />
